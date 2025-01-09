423ebd2f86707a34480a82bee5f21319de3425d0
unknown <maperaza2005@gmail.com>
2025-01-09 18:33:00 +0100

    Toque

--- lib/presentation/widgets/shared/custom_bottom_navigation.dart
+++ lib/presentation/widgets/shared/custom_bottom_navigation.dart
@@ -1,27 +1,36 @@
 import 'package:flutter/material.dart';
 
-
-class CustomBottomNavigation extends StatelessWidget {
+class CustomBottomNavigation extends StatefulWidget {
   const CustomBottomNavigation({super.key});
+
+  @override
+  _CustomBottomNavigationState createState() => _CustomBottomNavigationState();
+}
+
+class _CustomBottomNavigationState extends State<CustomBottomNavigation> {
+  int _currentIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return BottomNavigationBar(
+      currentIndex: _currentIndex,
+      onTap: _onItemTapped,
       elevation: 0,
+      type: BottomNavigationBarType.fixed,
       items: const [
+        BottomNavigationBarItem(icon: Icon(Icons.home_max), label: 'Inicio'),
         BottomNavigationBarItem(
-          icon: Icon( Icons.home_max ),
-          label: 'Inicio'
-        ),
+            icon: Icon(Icons.label_outline), label: 'Categorías'),
         BottomNavigationBarItem(
-          icon: Icon( Icons.label_outline ),
-          label: 'Categorías'
-        ),
-        BottomNavigationBarItem(
-          icon: Icon( Icons.favorite_outline ),
-          label: 'Favoritos'
-        ),
-      ]
+            icon: Icon(Icons.favorite_outline), label: 'Favoritos'),
+        BottomNavigationBarItem(icon: Icon(Icons.visibility), label: 'Por ver'),
+      ],
     );
   }
 }
--- lib/presentation/widgets/shared/full_screen_loader.dart
+++ lib/presentation/widgets/shared/full_screen_loader.dart
@@ -1,24 +1,20 @@
 import 'package:flutter/material.dart';
 
-
 class FullScreenLoader extends StatelessWidget {
   const FullScreenLoader({super.key});
-  
+
   Stream<String> getLoadingMessages() {
     final messages = <String>[
       'Cargando películas',
       'Comprando palomitas de maíz',
       'Cargando populares',
-      'Llamando a mi novia',
-      'Ya mero...',
       'Esto está tardando más de lo esperado :(',
     ];
 
-    return Stream.periodic( const Duration(milliseconds: 1200), (step) {
+    return Stream.periodic(const Duration(milliseconds: 1200), (step) {
       return messages[step];
     }).take(messages.length);
   }
-
 
   @override
   Widget build(BuildContext context) {
@@ -30,13 +26,12 @@ class FullScreenLoader extends StatelessWidget {
           const SizedBox(height: 10),
           const CircularProgressIndicator(strokeWidth: 2),
           const SizedBox(height: 10),
-
           StreamBuilder(
             stream: getLoadingMessages(),
             builder: (context, snapshot) {
-              if ( !snapshot.hasData ) return const Text('Cargando...');
-              
-              return Text( snapshot.data! );
+              if (!snapshot.hasData) return const Text('Cargando...');
+
+              return Text(snapshot.data!);
             },
           ),
         ],
