87e36328dd293635bb10401d0614789d6d2cfe1d
Migue <maperaza2005@gmail.com>
2025-01-10 08:35:57 +0100

    añadido funcionalidad de slide

--- lib/presentation/widgets/movies/movie_slideshow.dart
+++ lib/presentation/widgets/movies/movie_slideshow.dart
@@ -1,21 +1,19 @@
-import 'package:animate_do/animate_do.dart';
+import 'package:cinemapedia/presentation/screens/movies/movie_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:card_swiper/card_swiper.dart';
+import 'package:animate_do/animate_do.dart';
 import 'package:cinemapedia/domain/entities/movie.dart';
 
-
 class MoviesSlideshow extends StatelessWidget {
-  
   final List<Movie> movies;
-  
+
   const MoviesSlideshow({
-    super.key, 
-    required this.movies
+    super.key,
+    required this.movies,
   });
 
   @override
   Widget build(BuildContext context) {
-
     final colors = Theme.of(context).colorScheme;
 
     return SizedBox(
@@ -29,56 +27,63 @@ class MoviesSlideshow extends StatelessWidget {
           margin: const EdgeInsets.only(top: 0),
           builder: DotSwiperPaginationBuilder(
             activeColor: colors.primary,
-            color: colors.secondary
-          )
+            color: colors.secondary,
+          ),
         ),
         itemCount: movies.length,
-        itemBuilder: (context, index) => _Slide(movie: movies[index] ),
+        itemBuilder: (context, index) => _Slide(movie: movies[index]),
       ),
     );
   }
 }
 
-
 class _Slide extends StatelessWidget {
-
   final Movie movie;
 
   const _Slide({required this.movie});
 
   @override
   Widget build(BuildContext context) {
-
     final decoration = BoxDecoration(
       borderRadius: BorderRadius.circular(20),
       boxShadow: const [
         BoxShadow(
           color: Colors.black45,
           blurRadius: 10,
-          offset: Offset(0, 10)
-        )
-      ]
+          offset: Offset(0, 10),
+        ),
+      ],
     );
 
-    return Padding(
-      padding: const EdgeInsets.only( bottom: 30 ),
-      child: DecoratedBox(
-        decoration: decoration,
-        child: ClipRRect(
-          borderRadius: BorderRadius.circular(20),
-          child: Image.network(
-            movie.backdropPath,
-            fit: BoxFit.cover,
-            loadingBuilder: (context, child, loadingProgress) {
-              if ( loadingProgress != null ) {
-                return const DecoratedBox(
-                  decoration: BoxDecoration( color: Colors.black12 )
-                );
-              }
-              return FadeIn(child: child);
-            },
-          )
-        )
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (_) => MovieScreen(movieId: movie.id.toString()),
+          ),
+        );
+      },
+      child: Padding(
+        padding: const EdgeInsets.only(bottom: 30),
+        child: DecoratedBox(
+          decoration: decoration,
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(20),
+            child: Image.network(
+              movie.backdropPath,
+              fit: BoxFit.cover,
+              loadingBuilder: (context, child, loadingProgress) {
+                if (loadingProgress != null) {
+                  return const DecoratedBox(
+                    decoration: BoxDecoration(color: Colors.black12),
+                  );
+                }
+                return FadeIn(child: child);
+              },
+            ),
+          ),
+        ),
       ),
     );
   }
